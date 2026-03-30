15855ffc243edfa4e70abf52beb2b96ccdd4d372
kwonzihyun <kwonzihyun60@gmail.com>
2026-03-30 21:45:07 +0900

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCU1-QzLmmygXbhZwJztaTNSyN6vKpzgRI",
-  authDomain: "couple-app-e6812.firebaseapp.com",
-  projectId: "couple-app-e6812",
-  storageBucket: "couple-app-e6812.firebasestorage.app",
-  messagingSenderId: "289518730887",
-  appId: "1:289518730887:web:c2584e47aad78da84b0222"
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
